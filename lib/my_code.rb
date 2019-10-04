81eee74d2ef1ac81e7e047fabc4e94c0df705785
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-04 19:03:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  
+  i = 0 
+  while i < source_array.length
+    newArray[i] = source_array[i] ** 2
+    i+=1
+  end 
 end
 
 def reduce_to_all_true(source_array)
